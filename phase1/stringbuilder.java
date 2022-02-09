f2d2d6552e06870ef9653ade8962774fe27fa539
Ashish <59354086+ashishgollena@users.noreply.github.com>
2022-02-09 09:57:46 +0530

    phase2

---
+++ phase1/stringbuilder.java
@@ -0,0 +1,29 @@
+package com.training;
+
+public class stringbuilder {
+	
+	 
+	
+	 
+	    public static void main(String[] args) {
+	        
+	        String str = "String to StringBuilder StringBuffer";
+	        
+	        /*
+	         * To convert String to StringBuilder/StringBuffer
+	         * use the constructor.
+	         * 
+	         * public StringBuilder(String str) 
+	         * OR
+	         * public StringBuffer(String str)
+	         */
+	        
+	        StringBuilder sbl = new StringBuilder(str);
+	        System.out.println("StringBuilder contents: " + sbl);
+	        
+	        StringBuffer sbf = new StringBuffer(str);
+	        System.out.println("StringBuffer contents: " + sbf);
+	        
+	    }
+	}
+
